b6aaf4d888255a78cc75604bf676fcecbfe15144
Krum Yakimov <krum.yakimov@gmail.com>
2024-10-13 01:03:45 +0300

    feat: configure Flask app with environment settings and migrate user model
    - Configured the Flask app to support Development and Production environments using config.py - Added CORS support to the application for handling cross-origin requests - Set up Flask-Migrate and integrated database migrations with UserModel - Applied database configurations from environment variables via decouple - Mapped API resources in routes for client registration and login

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_migrate import Migrate
+from flask_restful import Api
+
+
+from db import db
+from resources.routes import routes
+
+app = Flask(__name__)
+app.config.from_object('config.DevelopmentConfig')
+db.init_app(app)
+
+api = Api(app)
+migrate = Migrate(app, db)
+
+CORS(app)
+
+
+@app.teardown_appcontext
+def close_request(response):
+    db.session.commit()
+    return response
+
+
+[api.add_resource(*route) for route in routes]
