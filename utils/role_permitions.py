44072af5b47891912d1fec87b1882ffbc8f019e3
Krum Yakimov <krum.yakimov@gmail.com>
2024-10-29 13:22:44 +0200

    chore(permissions): rename 'register' to 'create' and remove CLIENT role from role_permissions.py

--- utils/role_permitions.py
+++ utils/role_permitions.py
@@ -1,1 +1,23 @@
-git
+ROLE_PERMISSIONS = {
+    "ADMIN": {
+        "view": ["ADMIN", "APPROVER", "OWNER", "STAFF", "CLIENT"],
+        "create": ["ADMIN", "APPROVER", "OWNER", "STAFF"],
+        "edit": ["ADMIN", "APPROVER", "OWNER", "STAFF"],
+        "deactivate": ["ADMIN", "APPROVER", "OWNER", "STAFF", "CLIENT"],
+    },
+    "APPROVER": {
+        "view": ["OWNER", "STAFF"],
+        "create": ["OWNER", "STAFF"],
+        "edit": ["OWNER", "STAFF"],
+        "deactivate": ["OWNER", "STAFF"]
+    },
+    "OWNER": {
+        "view": ["STAFF"],
+        "create": ["STAFF"],
+        "edit": ["STAFF"],
+        "deactivate": ["STAFF"]
+    },
+    "STAFF": {
+        # Empty - staff cannot perform actions on other users
+    },
+}
